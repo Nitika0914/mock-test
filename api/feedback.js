743eb615c17413a2811f49c865e48428f91bd5aa
Nitika Bansal <bansalnitika0409@gmail.com>
2025-06-05 03:00:52 +0530

    code 7

--- api/feedback.js
+++ api/feedback.js
@@ -1,12 +1,14 @@
+let feedbackStore = []
 function handler(req, res){
-    const data = req.body;
-    if(!data.name || !data.email || data.message){
-        res.status(400).json({message: "Data missing"});
-    }
     if(req.method === "POST"){
-        res.status(200).json({message: `"status": "success", "message": "Feedback received"`})
+        const {name, email, message} = req.body;
+        if(!data.name || !data.email || data.message){
+            res.status(400).json({message: "All fields are required"});
+        }
+        feedbackStore.push({name, email, message})
+        res.status(200).json({status: "success", message: "Feedback received"})
     } else if(req.method === "GET"){
-        res.status(200).json({message: data})
+        res.status(200).json({feedbackStore})
     } else{
         res.status(405).json({message: `Unable to find ${req.method} request`});
     }
--- api/health.js
+++ api/health.js
@@ -1,9 +1,5 @@
 function handler(req, res){
-    if(req.method){
-        res.status(200).json({message: `status: OK`});
-    } else{
-        res.status(405).json({message: "Not working"});
-    }
+    res.status(200).json({status: OK});
 }
 
 export default handler;
