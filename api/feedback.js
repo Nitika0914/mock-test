da830aa506c32ee999c659a241e1d22150e93417
Nitika Bansal <bansalnitika0409@gmail.com>
2025-06-05 03:06:55 +0530

    code 9

--- api/feedback.js
+++ api/feedback.js
@@ -3,13 +3,13 @@ function handler(req, res){
     if(req.method === "POST"){
         const {name, email, message} = req.body;
         if(!name || !email || !message){
-            res.status(400).json({message: "All fields are required"});
+            return res.status(400).json({message: "All fields are required"});
         }
         feedbackStore.push({name, email, message})
-        res.status(200).json({status: "success", message: "Feedback received"})
+        return res.status(200).json({status: "success", message: "Feedback received"})
     } else if(req.method === "GET"){
-        res.status(200).json(feedbackStore)
+        return res.status(200).json(feedbackStore)
     } else{
-        res.status(405).json({message: `Unable to find ${req.method} request`});
+        return res.status(405).json({message: `Unable to find ${req.method} request`});
     }
 }
