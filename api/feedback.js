137c952778ef589b61c7cf3a6d5be41f968dab66
Nitika Bansal <bansalnitika0409@gmail.com>
2025-06-05 03:04:43 +0530

    code 8

--- api/feedback.js
+++ api/feedback.js
@@ -2,13 +2,13 @@ let feedbackStore = []
 function handler(req, res){
     if(req.method === "POST"){
         const {name, email, message} = req.body;
-        if(!data.name || !data.email || data.message){
+        if(!name || !email || !message){
             res.status(400).json({message: "All fields are required"});
         }
         feedbackStore.push({name, email, message})
         res.status(200).json({status: "success", message: "Feedback received"})
     } else if(req.method === "GET"){
-        res.status(200).json({feedbackStore})
+        res.status(200).json(feedbackStore)
     } else{
         res.status(405).json({message: `Unable to find ${req.method} request`});
     }
