b3a7591cceb4ac644a5201b21cfbf1cb234cec92
Nitika Bansal <bansalnitika0409@gmail.com>
2025-06-05 02:53:51 +0530

    code 5

--- api/feedback.js
+++ api/feedback.js
@@ -1,8 +1,8 @@
 function handler(req, res){
-    const data = req.body;
-    if(!data.name || !data.email || data.message){
-        res.status(400).json({message: "Data missing"});
-    }
+    // const data = req.body;
+    // if(!data.name || !data.email || data.message){
+    //     res.status(400).json({message: "Data missing"});
+    // }
     if(req.method === "POST"){
         res.status(200).json({message: `"status": "success", "message": "Feedback received"`})
     } else if(req.method === "GET"){
