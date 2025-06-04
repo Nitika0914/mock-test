a3d67a21308683bec83d428bd88241da6bfb2552
Nitika Bansal <bansalnitika0409@gmail.com>
2025-06-05 02:49:36 +0530

    code 4

--- api/health.js
+++ api/health.js
@@ -1,6 +1,6 @@
 function handler(req, res){
     if(req.method){
-        res.status(200).json({message: `"status": "OK"`});
+        res.status(200).json({message: `status: OK`});
     } else{
         res.status(405).json({message: "Not working"});
     }
